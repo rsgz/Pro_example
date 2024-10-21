42601b7ee38997a37742321e40a0faf90438a3e0
rsgz <52986175+rsgz@users.noreply.github.com>
2024-10-21 09:36:14 +0800

    Create 001.rs

---
+++ rust/Druid/悬浮框/悬浮框1/001.rs
@@ -0,0 +1,19 @@
+use druid::widget::prelude::*;
+use druid::widget::{Label, Flex};
+use druid::{AppLauncher, PlatformError, WindowDesc};
+
+fn main() -> Result<(), PlatformError> {
+    let main_window = WindowDesc::new(build_root_widget())
+        .title("Floating Window")
+        .window_size((200.0, 100.0)); // 设置窗口大小
+
+    AppLauncher::with_window(main_window)
+        .launch(())?;
+    
+    Ok(())
+}
+
+fn build_root_widget() -> impl Widget<()> {
+    Flex::column()
+        .with_child(Label::new("这是一个悬浮框"))
+}
